4d56ee41bc20e2ddebb9ef8839103bc10c0bbd7f
vagrant <vagrant@homestead>
2018-01-10 16:32:24 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,7 +18,7 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 	
-	public function store(Request $request)
+	public function store1(Request $request)
     {
 		
 		//$data = $request->all();
@@ -40,6 +40,43 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
 		//return redirect()->route('users.show', 1);
 		
+    }
+	
+	/*
+	public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+	*/
+	
+	public function store(Request $request)
+    {
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
+
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->route('users.show', [Auth::user()]);
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
+       }
     }
 	
 }
