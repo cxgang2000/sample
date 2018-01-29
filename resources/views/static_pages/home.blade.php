e1503b6e2a0998d854b03105cf9a1b0446e3b081
vagrant <vagrant@homestead>
2018-01-29 10:30:49 +0000

    Add user following

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,14 +5,17 @@
     <div class="row">
       <div class="col-md-8">
         <section class="status_form">
-          @include('shared.status_form')
+          @include('shared._status_form')
         </section>
         <h3>微博列表</h3>
-        @include('shared.feed')
+        @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+        <section class="stats">
+          @include('shared._stats', ['user' => Auth::user()])
         </section>
       </aside>
     </div>
@@ -28,6 +31,6 @@
       <p>
         <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
       </p>
-    </div>
+    </div>    
   @endif
 @stop
